e9d17fa0af753088894e820623c0cacb60aaa026
MathemagicalCalibrations <robinb299@gmail.com>
2020-08-09 14:00:03 +0200

    Improved mdm.py module Put SQL queries in try blocks. Added deletion, reevaluation and marking for revision.

--- mdm.py
+++ mdm.py
@@ -21,35 +21,91 @@ class MusicDataManager:
         self.db.close()
 
     def clean(self):
-        self.c.execute("""DROP TABLE IF EXISTS music""")
-        self.c.execute("""
-            CREATE TABLE music (
-            id INTEGER PRIMARY KEY,
-            path TEXT NOT NULL,
-            length INTEGER,
-            e INTEGER NOT NULL,
-            s INTEGER NOT NULL,
-            g INTEGER NOT NULL,
-            m INTEGER NOT NULL,
-            d INTEGER NOT NULL,
-            fix INTEGER
-            )
-        """)
+        try:
+            self.c.execute("""DROP TABLE IF EXISTS music""")
+            self.c.execute("""
+                CREATE TABLE music (
+                id INTEGER PRIMARY KEY,
+                path TEXT NOT NULL,
+                length INTEGER,
+                e INTEGER NOT NULL,
+                s INTEGER NOT NULL,
+                g INTEGER NOT NULL,
+                m INTEGER NOT NULL,
+                d INTEGER NOT NULL,
+                fix INTEGER
+                )
+            """)
+            self.db.commit()
+        except sqlite3.Error as e:
+            print(e)
 
     def add(self, path, length, e, s, g, m, d):
-        self.c.execute("""
-            INSERT INTO music
-            (path, length, e, s, g, m, d, fix)
-            values
-            (?, ?, ?, ?, ?, ?, ?, 0)""",
-            (path, length, e, s, g, m, d)
-        )
+        try:
+            self.c.execute("""
+                INSERT INTO music
+                (path, length, e, s, g, m, d, fix)
+                VALUES
+                (?, ?, ?, ?, ?, ?, ?, 0)""",
+                (path, length, e, s, g, m, d)
+            )
+            self.db.commit()
+        except sqlite3.Error as e:
+            print(e)
+
+    def delete(self, id):
+        try:
+            self.c.execute("""
+                DELETE FROM music
+                WHERE id = ?""",
+                (id, )
+            )
+        except sqlite3.Error as e:
+            print(e)
+
+    def reeval(self, id, e, s, g, m, d):
+        try:
+            self.c.execute("""
+                UPDATE music
+                SET e = ?, s = ?, g = ?, m = ?, d = ?, fix = 0
+                WHERE id = ?""",
+                (e, s, g, m, d, id)
+            )
+            self.db.commit()
+        except sqlite3.Error as e:
+            print(e)
+
+    def mark(self, id):
+        try:
+            self.c.execute("""
+                UPDATE music
+                SET fix = 1
+                WHERE id = ?""",
+                (id, )
+            )
+            self.db.commit()
+        except sqlite3.Error as e:
+            print(e)
+
+    def markedq(self):
+        try:
+            self.c.execute("""
+                SELECT *
+                FROM music
+                WHERE fix = 1
+            """)
+            return self.c.fetchall()
+        except sqlite3.Error as e:
+            print(e)
 
     def defaultq(self, et, st, gt, mt, dt, amount):
-        self.c.execute("""
-            SELECT rowid, path, length
-            FROM music
-            ORDER BY (e - ?)*(e - ?) + (s - ?)*(s - ?) + (g - ?)*(g - ?) + (m - ?)*(m - ?) + (d - ?)*(d - ?); """,
-            (et, et, st, st, gt, gt, mt, mt, dt, dt)
-        )
-        return self.c.fetchmany(amount)
+        try:
+            self.c.execute("""
+                SELECT rowid, path, length
+                FROM music
+                ORDER BY (e - ?)*(e - ?) + (s - ?)*(s - ?) + (g - ?)*(g - ?) + (m - ?)*(m - ?) + (d - ?)*(d - ?); """,
+                (et, et, st, st, gt, gt, mt, mt, dt, dt)
+            )
+            return self.c.fetchmany(amount)
+        except sqlite3.Error as e:
+            print(e)
