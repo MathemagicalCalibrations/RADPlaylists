ef2f39949ea54ce1ef6985dcb21b8a4fbb48f8d2
MathemagicalCalibrations <robinb299@gmail.com>
2020-08-19 18:53:10 +0200

    Added automatic queue setting to query wrappers in main and changed functions to read global q instead of getting it as parameter

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from pathlib import Path
 
 mdm = MusicDataManager(Path(cfg.get('path_settings', 'db_path')))
 dj = MusicPlayer(Path(cfg.get('path_settings', 'music_path')))
+q = []
 
 def queue(amount, et, st, gt, mt, dt, en = 0, ex = 5, ew = 1, sn = 0, sx = 5, sw = 1, gn = 0, gx = 5, gw = 1, mn = 0, mx = 5, mw = 1, dn = 0, dx = 5, dw = 1):
-    return mdm.queue(et, en, ex, ew, st, sn, sx, sw, gt, gn, gx, gw, mt, mn, mx, mw, dt, dn, dx, dw, amount)
+    global q
+    q = mdm.queue(et, en, ex, ew, st, sn, sx, sw, gt, gn, gx, gw, mt, mn, mx, mw, dt, dn, dx, dw, amount)
 
 def marked():
-    return mdm.marked()
+    global q
+    q = mdm.marked()
+
+def getqueue():
+    for song in q:
+        print(song)
 
 def mark(id):
     mdm.mark(id)
@@ -23,12 +30,23 @@ def addsong(path, e, s, g, m, d):
     mdm.add(path, e, s, g, m, d)
 
 def deletesong(id):
-    mdm.delete(id)
+    confirm = input("Are you sure? (y/N)")
+    if confirm = 'y' or confirm = 'Y':
+        mdm.delete(id)
+    else:
+        print("Aborted \n")
 
 def index():
     return dj.getindex()
 
-def play(q = None):
+def currentsong():
+    i = index()
+    if i == None:
+        print("No song is playing \n")
+        return
+    return q[index()]
+
+def play():
     dj.setqueue(q)
     dj.start()
 
